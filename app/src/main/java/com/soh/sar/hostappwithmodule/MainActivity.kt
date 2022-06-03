8096c1b1bd5093cf9f732ea68c2ed1698876fb6b
sohan <sohan.shetty111@gmail.com>
2022-06-03 11:15:20 +0530

    Added materialTextView Added proguard

--- app/src/main/java/com/soh/sar/hostappwithmodule/MainActivity.kt
+++ app/src/main/java/com/soh/sar/hostappwithmodule/MainActivity.kt
@@ -3,6 +3,7 @@ package com.soh.sar.hostappwithmodule
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sohan.customcalender.ui.BlankActivity
 import com.example.sohan.customcalender.ui.LandingActivity
 
 class MainActivity : AppCompatActivity() {
